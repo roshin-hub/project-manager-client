884193b8e1a25e3b78d219992fdd223a07d784f7
roshin-hub <roshin_007@yahoo.co.in>
2019-04-09 00:42:36 -0400

    Added projcet page changes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { ServerConfig } from '../environments/environment';
+import { AppError } from './models/app.error';
+import { Product } from './models/product';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  server = new ServerConfig();
+  appError: AppError = new AppError();
 
   constructor(private http: HttpClient) { }
 
@@ -19,4 +33,23 @@ export class DataService {
   getProjects() {
     return this.http.get('http://localhost:8080/project');
   }
+
+  /*createProject(data) {
+    headers= " { 'Content-Type': 'application/x-www-form-urlencoded' }"
+    return this.http.post('http://localhost:8080/project/create', data,headers) 
+
+  }*/
+
+  createProject(product: Product): Observable<Product | AppError> {
+    console.log(product)
+    console.log(this.server.url)
+    
+    return this.http.post<Product>('http://localhost:8080/project/create', product, httpOptions)
+     .pipe(             
+        //catchError(err => this.appError.handleError(err))
+        catchError(err => this.appError.handleError(err))        
+      );
+      
+  }  
+  
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-project',
@@ -9,9 +10,12 @@ import { DataService } from '../data.service';
 })
 export class ProjectComponent implements OnInit {
 
+ 
   messageForm: FormGroup;
   submitted = false;
   success = false;
+  resptxt = '';
+  
 
   columnDefs = [
         {headerName: 'Title', field: 'title',sortable: true ,filter: true},        
@@ -25,8 +29,8 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit() {
     this.messageForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      message: ['', Validators.required]
+      title: ['', Validators.required],
+      priority: ['', Validators.required]
     });
 
     /*fetch('https://api.myjson.com/bins/15psn9')
@@ -47,7 +51,58 @@ export class ProjectComponent implements OnInit {
         return;
     }
 
+
+    let product = new Product();
+    product.title = this.messageForm.value.title;
+    product.priority = this.messageForm.value.priority;  
+
+    this.service.createProject(product).subscribe((result) => {
+    console.log("This code will be executed when the HTTP call returns successfully")
+     });
+    
+     /*this.orderSvc.addItemToCart(cartItem).subscribe((res: OrderProduct) => {
+      if (res instanceof AppError) {
+        this.showError(res.message);
+        return;
+      }
+      else
+        this.router.navigateByUrl('/authenticated/cart');      
+    }, err => {
+      this.showError('Failed to add item to the cart!');
+      console.log(err);
+    }); */   
+    /*this.orderSvc.addItemToCart(cartItem).subscribe((res: OrderProduct) => {
+      if (res instanceof AppError) {
+        this.showError(res.message);
+        return;
+      }
+      else
+        this.router.navigateByUrl('/authenticated/cart');      
+    }, err => {
+      this.showError('Failed to add item to the cart!');
+      console.log(err);
+    }); */   
+
+    //console.log(this.messageForm.value);
+    this.service.createProject(this.messageForm.value)
+    //console.log(this.resptxt);
+    alert('The form was submitted');
+    this.service.getProjects().subscribe(data => {
+        this.rowData = data        
+      });
+    this.messageForm.reset();
+
     this.success = true;
+
+    location.reload()
+
+    /*this.rootService.postAPIData().subscribe((response)=>{
+      console.log('response from post data is ', response);
+    },(error)=>{
+      console.log('error during post is ', error)
+    })*/
+
+
 }
 
 
